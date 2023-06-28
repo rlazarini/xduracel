3f9546b1a5608a70b016ab710f159bb04af68fc8
Rafael Lazarini <rlazarini@gmail.com>
2023-06-27 23:52:07 -0300

    feat: adicionado componente de medias

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,8 +2,26 @@
   <div class="grid grid-cols-3 gap-4 portrait:grid-cols-1">
     <div class="portrait:order-last portrait:hidden">
     </div>
-    <div class="h-screen col-span-2 border-2 border-yellow-500">
-      b
+    <div class="h-full col-span-2">
+      <div class="grid grid-cols-4 gap-4 text-white portrait:grid-rows-2 portrait:grid-cols-none">
+        <div class="block-content p-4 landscape:col-span-3 stroke-gray-22550">
+          Fala galera.... beleza?
+        </div>
+        <div class="portrait:order-first text-center">
+          <Medias />
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import Medias from "@/components/Medias.vue";
+</script>
+
+<style scoped>
+.block-content {
+  text-shadow: -1px -1px 0 rgba(0, 0, 0, 0.3), 1px -1px 0 rgba(0, 0, 0, 0.3),
+    -1px 1px 0 rgba(0, 0, 0, 0.3), 1px 1px 0 rgba(0, 0, 0, 0.3);
+}
+</style>
---
+++ src/components/Medias.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="flex flex-col items-center justify-center portrait:flex-row portrait:justify-around">
+    <li
+      v-for="(sm, index) in socialMediaList"
+      :key="`${tipo}_${index}`"
+      class="py-4"
+    >
+      <a
+        :href="sm.link"
+        target="_blank"
+        class="flex flex-col items-center justify-center"
+      >
+        <img
+          :src="`/icons/icon-${sm.tipo}.png`"
+          alt=""
+          class="max-w-[48px] portrait:max-w-[36px]"
+        >
+        <span class="text-sm portrait:text-xs portrait:font-bold">{{ sm.desc }}</span>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const socialMediaList = ref([
+  {
+    tipo: "instagram",
+    desc: "Instagram",
+    link: "https://www.instagram.com/duracelgaming/",
+  },
+  {
+    tipo: "youtube",
+    desc: "xDuracel",
+    link: "https://www.youtube.com/@xDuracelx",
+  },
+  {
+    tipo: "youtube",
+    desc: "xDuracelx",
+    link: "https://www.youtube.com/@xDuracelx1",
+  },
+  {
+    tipo: "livepix",
+    desc: "Livepix",
+    link: "https://livepix.gg/xduracel",
+  },
+]);
+</script>
