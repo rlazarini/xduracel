e6c590e8c6ad6247164b588b39058f9732a3053f
Rafael Lazarini <rlazarini@gmail.com>
2023-07-21 18:15:55 -0300

    fix: Removida as bordas usadas como delimitador de marcação

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,7 +3,7 @@
     <div class="portrait:order-last portrait:hidden">
     </div>
     <div class="h-full col-span-3">
-      <div class="grid grid-cols-6 gap-4 text-white portrait:grid-rows-2 portrait:grid-cols-none border-yellow-400">
+      <div class="grid grid-cols-6 gap-4 text-white portrait:grid-rows-2 portrait:grid-cols-none">
         <div class="block-content p-4 landscape:col-span-5 stroke-gray-22550">
           Fala galera.... beleza?
         </div>
@@ -20,9 +20,6 @@ import Medias from "@/components/Medias.vue";
 </script>
 
 <style scoped>
-* {
-  border: 1px solid red;
-}
 .block-content {
   text-shadow: -1px -1px 0 rgba(0, 0, 0, 0.3), 1px -1px 0 rgba(0, 0, 0, 0.3),
     -1px 1px 0 rgba(0, 0, 0, 0.3), 1px 1px 0 rgba(0, 0, 0, 0.3);
