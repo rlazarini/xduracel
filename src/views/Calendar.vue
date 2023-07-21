7f3fb3bc20db88113a83f616b4b641a36c25f542
Rafael Lazarini <rlazarini@gmail.com>
2023-07-21 18:59:38 -0300

    fix: Exibição do calendario em dispositivos diferentes obedecendo um tamanho max

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -5,15 +5,16 @@
       <div class="portrait:order-last portrait:hidden">
       </div>
       <div class="h-full col-span-2">
-        <div class="grid grid-cols-1 gap-4 text-white portrait:grid-rows-2 portrait:grid-cols-none border-yellow-400">
-          <iframe
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23E4C441&ctz=America%2FAraguaina&showTitle=0&showNav=1&showDate=1&showTabs=0&showPrint=0&showCalendars=0&showTz=0&hl=pt_BR&src=Yzg4NDgyYTYzNjhjOGRlOWY3ODY1YTdjMmRiYjI0ODBlNzIxMmQ2NjQ5YTYyOTE5ZTQ4MzViZjU3MzE3ODEyOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=cHQuYnJhemlsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23E67C73&color=%230B8043"
-            style="border-width:0"
-            width="800"
-            height="600"
-            frameborder="0"
-            scrolling="no"
-          ></iframe>
+        <div class="grid grid-cols-1 gap-4 text-white portrait:grid-rows-2 portrait:grid-cols-none">
+          <div class="w-full portrait:w-auto portrait:my-0 portrait:mx-auto">
+            <iframe
+              src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23E4C441&ctz=America%2FAraguaina&showTitle=0&showNav=1&showDate=1&showTabs=0&showPrint=0&showCalendars=0&showTz=0&hl=pt_BR&src=Yzg4NDgyYTYzNjhjOGRlOWY3ODY1YTdjMmRiYjI0ODBlNzIxMmQ2NjQ5YTYyOTE5ZTQ4MzViZjU3MzE3ODEyOUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=cHQuYnJhemlsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23E67C73&color=%230B8043"
+              style="border-width:0"
+              frameborder="0"
+              scrolling="no"
+              class="w-[95%] h-[60vh] portrait:w-[80vw] portrait:h-[60vh]"
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
